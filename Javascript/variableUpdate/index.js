f84a05f13c93ff8d882402e887884ef49bc5944b
greenwhale00 <goreakay@gmail.com>
2023-10-26 20:20:57 +0900

    231026

---
+++ Javascript/variableUpdate/index.js
@@ -0,0 +1,24 @@
+let numLives = 9;
+undefined
+numLives
+9
+numLives -= 1;
+8
+numLives
+8
+numLives--;
+8
+numLives
+7
+numLives
+7
+numLives
+7
+numLives--;
+7
+numLives
+6
+numLives++;
+6
+numLives
+7
