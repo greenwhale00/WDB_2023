67d4a44096e37f66344db4efb990548e07316d16
greenwhale00 <goreakay@gmail.com>
2023-10-27 11:05:57 +0900

    231027

---
+++ Javascript/stringVariablesTest/index.js
@@ -0,0 +1,10 @@
+//다음 문자열 변수를 정의하세요. (let 또는 const 중 하나를 사용할 수 있습니다.)
+
+//bestColor 는 문자열purple (우리 모두가 알다시피 지구상에서 가장 좋은 색상)로 설정해야 합니다.
+
+//quote는 문자열 You had me at "hello"로 설정해야 합니다. 따옴표에 주의하세요. 문자열 안에는 큰따옴표가 필요합니다.
+
+
+let bestColor = 'purple';
+
+let quote = 'You had me at "hello"'
