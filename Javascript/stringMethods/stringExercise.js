9b2d700c44dbbc8b9e4c778fefb7f729cabec926
greenwhale00 <goreakay@gmail.com>
2023-10-30 11:59:40 +0900

    231030

---
+++ Javascript/stringMethods/stringExercise.js
@@ -0,0 +1,9 @@
+// 1. 문자열 메서드를 사용하여 "skateboard"에서 "board" 부분을 추출합니다(slice 메서드 사용).
+// 2. board의 'o'를 'e'로 바꿉니다(replace 메서드 사용).
+// 3. 결과를 facialHair라는 변수에 저장합니다.
+
+
+
+const word = "skateboard";
+
+const facialHair = word.slice(5, 10).replace('o', 'e');
