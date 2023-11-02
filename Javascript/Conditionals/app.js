57e58b067c87ade2e463c7e09673ce36a81b7736
greenwhale00 <goreakay@gmail.com>
2023-11-02 10:48:51 +0900

    231102

--- Javascript/Conditionals/app.js
+++ Javascript/Conditionals/app.js
@@ -1,1 +1,14 @@
-console.log("IT's working!!!!");
+console.log("BEFORE CONDITIONALS");
+let random = Math.random()
+
+if (random < 0.5) {
+  console.log("Your number is less than 0.5!!!!");
+  console.log(random)
+}
+
+if (random >= 0.5) {
+  console.log("Your number is greater than 0.5!!!!");
+  console.log(random)
+}
+
+console.log("AFTER CONDITIONALS");
