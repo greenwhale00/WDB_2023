a5d108786dca062fc064e4d6515021bd2e87167a
greenwhale00 <goreakay@gmail.com>
2023-11-03 11:26:35 +0900

    231103

--- Javascript/Conditionals/app.js
+++ Javascript/Conditionals/app.js
@@ -1,14 +1,25 @@
-console.log("BEFORE CONDITIONALS");
-let random = Math.random()
+// console.log("BEFORE CONDITIONALS");
+// let random = Math.random()
 
-if (random < 0.5) {
-  console.log("Your number is less than 0.5!!!!");
-  console.log(random)
-}
+// if (random < 0.5) {
+//   console.log("Your number is less than 0.5!!!!");
+//   console.log(random)
+// }
 
-if (random >= 0.5) {
-  console.log("Your number is greater than 0.5!!!!");
-  console.log(random)
-}
+// if (random >= 0.5) {
+//   console.log("Your number is greater than 0.5!!!!");
+//   console.log(random)
+// }
 
-console.log("AFTER CONDITIONALS");
+// console.log("AFTER CONDITIONALS");
+
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//   console.log('Ughhh I hate mondays......')
+// } else if (dayOfWeek === 'Saturday') {
+//   console.log('YAY!!!! I LOVE SATURDAYS!!!!!!!!')
+// } else if (dayOfWeek === 'Friday') {
+//   console.log('Fridays are decent, especially after work')
+// }
